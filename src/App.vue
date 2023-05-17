55e359ac9f9efae919635f582dedd7301c5b895f
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-05-17 15:14:30 -0300

    remove lógica duplicada, acrescenta links e troca informações com o RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,207 +1,27 @@
 <script>
 import { RouterLink, RouterView } from "vue-router";
+import "@/assets/base.css";
 
 export default {
   data() {
     return {
-      posts: [
-        {
-          title: "Meu primeiro post",
-          datetime: Date.now(),
-          content: "Postar aqui é muito legal",
-        },
-        {
-          title: "Meu segundo post",
-          datetime: Date.now(),
-          content: "Postar aqui não é tão legal",
-        },
-      ],
-      FormData: {
-        title: "",
-        content: "",
-      },
-      search:"",
+      posts: [],
     };
   },
-  computed: {
-    filteredPosts (){
-      // se search estiver vazio, retorne a lista completa de posts
-      if (!this.search) return this.posts;
-
-      // se tiver qualquer coisa em search, faz o filtro
-      const listaFinal = [];
-      for (const post of this.posts) {
-        if (post.title.includes(this.search)) {
-          listaFinal.push(post);
-        }
-      }
-      return listaFinal;
-    },
-  },
   methods: {
-    handleClick(event) {
-      const now = new Date();
-      const dataDaPostagem = `${now.getDate()}/${
-        now.getMonth() + 1
-      }/${now.getFullYear()}`;
-
-      // metodo 1
-      // this.post[this.posts.length] = {
-      //   title: this.FormData.title
-      //   content: this;this.FormData.content
-      // };
-
-      // metodo 2
-      this.posts.push({
-        title: this.FormData.title,
-        content: this.FormData.content,
-        datetime: dataDaPostagem,
-      });
-
-      this.FormData = {
-        title: "",
-        content: "",
-      };
-    },
-    handleInputChange(event) {
-      const { name, value } = event.target;
-      // essa função de cima é igual fazer essa de baixo só q mais sucinto.
-      // event.target.name
-      // event.target.value
-
-      this.FormData[name] = value;
+    addPost(newPost) {
+      //adicionar o novo post à lista de posts
+      this.posts.push(newPost);
     },
   },
 };
 </script>
 
 <template>
-  <input v-model="search" @input="" placeholder="Procure pelo título do post..." />
+  <RouterLink to="/"> Home </RouterLink>
+  <RouterLink to="/create"> Novo Post</RouterLink>
 
-  <div id="lista-posts">
-    <div class="post" v-for="post in filteredPosts" :key="post.title">
-      <h3>{{ post.title }}</h3>
-      <h4>{{ post.datetime }}</h4>
-      <p>{{ post.content }}</p>
-    </div>
-  </div>
-
-  <form action="">
-    <!-- essa @keyup = handleInputChange é pra trata os inputs que coloca nos campos pra nao cuidar td de uma vez quando da submit. -->
-    <input name="title" v-model="FormData.title" placeholder="Título" />
-    <textarea
-      name="content"
-      :value="FormData.content"
-      @keyup="handleInputChange"
-      id=""
-      cols="30"
-      rows="10"
-      placeholder="Escreva seu post aqui ..."
-    ></textarea>
-    <button class="learn-more" type="button" @click="handleClick">Criar</button>
-  </form>
-
-  <RouterView />
+  <RouterView :posts="posts" @create-post="addPost" />
 </template>
 
-<style scoped>
-* {
-  font-family: "Poppins", sans-serif;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-form > * {
-  margin: 1rem;
-  display: flex;
-}
-
-/* form:focus {
-  color: red;
-} */
-
-button > * {
-  box-sizing: border-box;
-}
-
-button > ::before,
-::after {
-  box-sizing: border-box;
-}
-
-button {
-  font-family: "Rubik", sans-serif;
-  font-size: 1rem;
-  line-height: 1.5;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 0;
-  max-width: 20vh;
-  max-height: 20vh;
-  background: #fff;
-
-  position: relative;
-  display: inline-block;
-  cursor: pointer;
-  outline: none;
-  border: 0;
-  vertical-align: middle;
-  text-decoration: none;
-  font-size: inherit;
-  font-family: inherit;
-}
-
-.learn-more {
-  font-weight: 600;
-  color: #382b22;
-  text-transform: uppercase;
-  padding: 1.25em 2em;
-  background: #fff0f0;
-  border: 2px solid #b18597;
-  border-radius: 0.75em;
-  transform-style: preserve-3d;
-  transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
-    background 150ms cubic-bezier(0, 0, 0.58, 1);
-}
-
-button::before {
-  position: absolute;
-  content: "";
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: #f9c4d2;
-  border-radius: inherit;
-  box-shadow: 0 0 0 2px #b18597, 0 0.625em 0 0 #ffe3e2;
-  transform: translate3d(0, 0.75em, -1em);
-  transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
-    background 150ms cubic-bezier(0, 0, 0.58, 1);
-}
-
-button:hover {
-  background: #ffe9e9;
-  transform: translate(0, 0.25em);
-}
-
-button:hover::before {
-  box-shadow: 0 0 0 2px #b18597, 0 0.5em 0 0 #ffe3e2;
-  transform: translate3d(0, 0.5em, -1em);
-}
-
-button:active {
-  background: #ffe9e9;
-  transform: translate(0em, 0.75em);
-}
-
-button:active::before {
-  box-shadow: 0 0 0 2px #b18597, 0 0 #ffe3e2;
-  transform: translate3d(0, 0, -1em);
-}
-</style>
+<style scoped></style>
